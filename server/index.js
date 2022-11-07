17040b0bcf300ebc5e466022f762dc4285607cd5
Youssef Ragab <ragab.youssef95@gmail.com>
2022-11-07 09:27:02 -0500

    outline routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
 const PORT = 8080;
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES
+
+//Create post
+
+//Comment on post
+
+//React to post
+
+//Delete post
+
+//Delete comment
+
+//Delete reaction
+
+//Change reaction
 
 app.listen(8080, () => {
   console.log(`Server has started on port ${PORT} 👍`);
